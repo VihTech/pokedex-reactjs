49ee532757a8c0d7e0013859143b5a21983457fb
Vitória Maria <109771542+VitoriaMariadev@users.noreply.github.com>
2023-06-14 14:39:30 -0300

    FEAT/ Iniciando a tela de cadastrar usuário

--- pokedex/src/Pages/cadastrar_usuario/index.jsx
+++ pokedex/src/Pages/cadastrar_usuario/index.jsx
@@ -1,11 +1,81 @@
 import Header from "../../Components/Header"
+import "./style.css"
+import {BsPersonCircle, BsFillPersonFill, BsEyeFill, BsEyeSlashFill} from 'react-icons/bs'
 
 export const LogarUsuario = () =>{
     return(
         <div>
             <Header/>
             <main className="main-cadastrar-usuario">
-                <div className="main-cadastrar-usuario-conteiner"></div>
+                <div className="main-cadastrar-usuario-conteiner">
+                    <div className="main-cadastrar-usuario-conteiner-vermelho"></div>
+
+                    <div className="main-cadastrar-usuario-conteiner-branco">
+
+                        <div className="main-cadastrar-usuario-conteiner-informacoes">
+                            <div className="main-cadastrar-usuario-conteiner-informacoes-titulo">
+                                <h2>Cadastrar</h2>    
+                            </div> 
+
+                            <div className="main-cadastrar-usuario-conteiner-informacoes-container">
+                                <div className="main-cadastrar-usuario-conteiner-informacoes-container-img"><BsPersonCircle/></div>
+                                <div className="main-cadastrar-usuario-conteiner-informacoes-container-formulario">
+
+                                    <div className="main-cadastrar-usuario-conteiner-informacoes-container-formulario-nome">
+                                        <div className="main-cadastrar-usuario-conteiner-informacoes-container-formulario-nome-titulo">
+                                            <p>Nome</p>
+
+                                            <div className="main-cadastrar-usuario-conteiner-informacoes-container-formulario-nome-titulo-container">
+                                                <div className="main-cadastrar-usuario-conteiner-informacoes-container-formulario-nome-titulo-container-svg"><BsFillPersonFill/></div>
+                                                <input type="text" />
+                                            </div>
+
+                                        </div>
+                                    </div>
+
+                                    <div className="main-cadastrar-usuario-conteiner-informacoes-container-formulario-senha">
+
+                                        <div className="main-cadastrar-usuario-conteiner-informacoes-container-formulario-senha-titulo">
+                                            <p>Senha</p>
+
+                                            <div className="main-cadastrar-usuario-conteiner-informacoes-container-formulario-senha-titulo-container">
+
+                                                <div className="main-cadastrar-usuario-conteiner-informacoes-container-formulario-senha-titulo-container-svg"><BsEyeSlashFill/></div>
+                                                <input type="password" />
+
+                                            </div>
+
+
+                                        </div>
+
+                                    </div>
+                                    <div className="main-cadastrar-usuario-conteiner-informacoes-container-formulario-confirmasenha">
+                                        
+
+                                        <div className="main-cadastrar-usuario-conteiner-informacoes-container-formulario-confirmasenha-titulo">
+                                            <p>Confirmar Senha</p>
+
+                                            <div className="main-cadastrar-usuario-conteiner-informacoes-container-formulario-confirmasenha-titulo-container">
+
+                                                <div className="main-cadastrar-usuario-conteiner-informacoes-container-formulario-confirmasenha-titulo-container-svg"><BsEyeSlashFill/></div>
+                                                <input type="password" />
+
+                                            </div>
+
+                                        </div>
+
+                                    </div>
+                                </div>
+                                <div className="main-cadastrar-usuario-conteiner-informacoes-container-bnt">
+
+                                    <button>Cadastrar</button>
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
             </main>
         </div>
     )
